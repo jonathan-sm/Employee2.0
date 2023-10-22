a6c912d5ddfcfbe77c7cba113f32eb25f4e83cb4
Jonathan-sm <Jonathan_sm@mail.ru>
2023-10-22 20:10:02 +0300

    скачали библеотеку

---
+++ src/main/java/Employee/New/exception/EmployeeAlreadyAddedException.java
@@ -0,0 +1,11 @@
+package Employee.New.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpStatusCodeException;
+
+public class EmployeeAlreadyAddedException extends HttpStatusCodeException {
+
+    public EmployeeAlreadyAddedException(String message) {
+        super(HttpStatus.BAD_REQUEST, message);
+    }
+}
